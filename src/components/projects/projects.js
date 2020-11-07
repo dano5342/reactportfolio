0718298a72b33ad43937fcf09ffc1e797bcaca0e
dano5342 <ellis.daniel0@gmail.com>
2020-11-07 12:11:33 +0100

    added images for display, thumbnails are being styled next.

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,17 +1,47 @@
 import React from 'react'
-import Thumbnail from '../thumbnail/thumbnail.js';
 
+
+import Thumbnail from '../thumbnail/thumbnail.js';
+import hadageto from '../../assets/images/hadageto.png';
+import ms2 from '../../assets/images/ms2.png';
+import ms3 from '../../assets/images/ms3.png';
+import ms4 from '../../assets/images/ms4.png';
 import "./projects.css"
 
 export default function Projects() {
     return (
-        <div>
+        <div className="projects-container">
             <h1>Projects</h1>
+            <Thumbnail
+                className="hadageto"
+                link="/hadageto"
+                image={hadageto}
+                title="Hadageto"
+                category="Work"
+            />
+
+            <Thumbnail
+                className="project1"
+                link="/milestone4"
+                image={ms4}
+                title="Artefacts and Curiose"
+                category="Learning"
+            />
+
+            <Thumbnail
+                className="project2"
+                link="/milestone3"
+                image={ms3}
+                title="Cookbook"
+                category="Learning"
+            />
+
             <Thumbnail 
-                link=""
-                image=""
-                title=""
-                category=""
+                className="project3"
+                link="/milestone2"
+                image={ms2}
+                title="Lord of the Rings Dashboard"
+                category="Learning"
             />
         </div>
     )
